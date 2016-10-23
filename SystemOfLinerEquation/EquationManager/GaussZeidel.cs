ea0ddf06a89ac8955c266027ffef39a430b2ff0e
denistimchenko <denistimchenko@DENISTIMCHE991B>
2016-10-23 20:35:07 +0300

    Gauss-Zeidel

---
+++ SystemOfLinerEquation/EquationManager/GaussZeidel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquationManager {
+    public class GaussZeidel {
+        public double Epsilon = 0.01;
+        public int MatrixSize;
+        public int K;
+        public int NumberOfIterations;
+        public double S;
+        public double Xi;
+        public double Diff = 1;
+        public double[,] Matrix;
+        public double[] Value;
+        public double[] Roots;
+
+        public GaussZeidel(double[,] matrix, double[] value, int numberOfIterations, int matrixSize, double[] roots) {
+            Matrix = matrix;
+            NumberOfIterations = numberOfIterations;
+            Value = value;
+            MatrixSize = matrixSize;
+            Roots = roots;
+        }
+
+        public double[] Algoritm() {
+            K = 0;
+            while ((K <= NumberOfIterations) && (Diff >= Epsilon)) {
+                K = K + 1;
+                for (var i = 0; i < MatrixSize; i++) {
+                    S = 0;
+                    for (var j = 0; j < MatrixSize; j++) {
+                        if (i != j) {
+                            S += Matrix[i, j] * Roots[j];
+                        }
+                    }
+                    Xi = (Value[i] - S) / Matrix[i, i];
+                    Diff = Math.Abs(Xi - Roots[i]);
+                    Roots[i] = Xi;
+                }
+            }
+            return Roots;
+        }
+    }
+}
--- SystemOfLinerEquation/Gauss.output/Program.cs
+++ SystemOfLinerEquation/Gauss.output/Program.cs
@@ -1,22 +1,56 @@
 ﻿using System;
+using EquationManager;
 
 namespace Gauss.output {
     internal class Program {
         private static void Main(string[] args) {
+            var n = 3;
+            var b = new double[n];
+            var x = new double[n];
 
-            var manager = new EquationManager.IterationManager();
+
+            var manager = new IterationManager();
             var matrix = new double[3, 4];
 
             var fielledMatrix = manager.GetMatrix(matrix);
             var matrixWithLeadingElement = manager.GetLeadingElement(fielledMatrix);
             var triangleMatrix = manager.DirectFlow(matrixWithLeadingElement, 3);
+
             Console.WriteLine("Triangle Matrix: \n");
             manager.ShowMatrix(triangleMatrix);
             Console.WriteLine();
+
             Console.WriteLine("Solutions:\n");
             var vals = manager.ReverseFlow(triangleMatrix, 3, 3);
             for (var i = 0; i < vals.Length; i++) {
-                Console.WriteLine($"x{i+1}={vals[i]}\n");
+                // Console.WriteLine($"x{i+1}={vals[i]}\n");
+                Console.WriteLine(string.Format("x{0}={1}\n", i + 1, vals[i]));
+            }
+
+            Console.WriteLine("Gauss - Zeidel");
+
+            for (var i = 0; i < n; i++) {
+                x[i] = 0;
+            }
+
+            //TODO: Split matrix into A and B 
+            var aMatrix = new double[3, 3];
+            var bMatrix = new double[3];
+            for (var i = 1; i < matrix.GetLength(1); i++) {
+                for (var j = 1; j < matrix.GetLength(1); j++) {
+
+                    aMatrix[i - 1, j - 1] = matrix[i - 1, j - 1];
+                }
+
+                bMatrix[i - 1] = matrix[i - 1, matrix.GetLength(0)];
+
+            }
+
+
+            var test = new GaussZeidel(aMatrix, bMatrix, 500, n, x);
+            var results = test.Algoritm();
+            for (var i = 0; i < results.Length; i++) {
+                Console.WriteLine(string.Format("x{0}={1}\n", i + 1, results[i]));
             }
             Console.ReadLine();
         }
