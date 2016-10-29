f974cd8dffccf94ba4dd9dd6e37c67263c53d217
denistimchenko <denistimchenko@DENISTIMCHE991B>
2016-10-29 21:52:31 +0300

    Add mahaps themes

--- STOCK.MNG.SYSTEM/Stock.Gui/MainWindow.xaml.cs
+++ STOCK.MNG.SYSTEM/Stock.Gui/MainWindow.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System.Windows;
+using MahApps.Metro.Controls;
 
 namespace Stock.Gui {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
+    public partial class MainWindow : MetroWindow {
         public MainWindow() {
             InitializeComponent();
             DataContext = new vpoutput.MainViewModel();
